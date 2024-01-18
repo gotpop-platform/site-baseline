e00851f1bab714235e683475de7b01e9d23edf25
GotPop <liam@gotpop.net>
2024-01-18 02:39:55 +0000

    Update Article component and index.html

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -3,12 +3,15 @@ import { useCSS } from "src/hooks/useCSS"
 
 type ArticleProps = { title: string; content: string }
 
+export const useName = import.meta.file.split(".").shift()?.toLowerCase()
+
 export const Article = ({ title, content }: ArticleProps) => {
   const { css } = useCSS(import.meta)
+  const path = "src/components/Article/ArticleClient.js"
 
   return (
-    <article class-name="liam">
-      <script src="./ArticleClient.ts" type="module"></script>
+    <article class-name={useName}>
+      <script src={path} type="module"></script>
       <style>{css}</style>
       <h2>{title}</h2>
       <p>{content}</p>
