9b1cf8bd266692ae451dda8f809baaee8f019f30
GotPop <liam@gotpop.net>
2024-01-19 22:07:51 +0000

    Refactor build.tsx to improve logging and display build outputs

--- build.tsx
+++ build.tsx
@@ -1,13 +1,11 @@
-import { Glob } from "bun"
+import { Glob, type BuildArtifact } from "bun"
 
 async function liam() {
   const glob = new Glob("**/*.client.ts")
-  console.log("glob :", glob.scan("."))
 
   let arr = []
-  // Scans the current working directory and each of its sub-directories recursively
   for await (const file of glob.scan(".")) {
-    console.log(file)
+    // console.log(file, glob.scan("."))
     arr.push(file)
   }
 
@@ -18,11 +16,44 @@ async function liam() {
 
   const style = `\x1b[32m%s\x1b[0m`
   const error = `\x1b[31m%s\x1b[0m`
+  const orange = `\x1b[33m%s\x1b[0m`
+  const purple = `\x1b[35m%s\x1b[0m`
+  const greyAndBold = `\x1b[1m\x1b[30m%s\x1b[0m`
+  const truckEmoji = `\x1b[1m\x1b[30m🚚\x1b[0m`
+  const checkEmoji = `\x1b[1m\x1b[30m✅\x1b[0m`
+  const hashEmoji = `\x1b[1m\x1b[30m📦\x1b[0m`
+  const greenCheckEmoji = `\x1b[1m\x1b[30m✅\x1b[0m`
+  const redXEmoji = `\x1b[1m\x1b[30m❌\x1b[0m`
 
   if (build.success) {
-    console.log(style, "Build succeeded")
+    // console.log("build.outputs :", build.outputs)
+
+    const root = process.cwd().toString()
+    for await (const output of build.outputs) {
+      const liam = output.path.replace(root + "/dist", "")
+
+      // Create an object with the properties you want to display
+      const tableData = {
+        hash: output.hash,
+        output: liam,
+      }
+
+      // Log the object as a table
+      console.table(tableData)
+    }
+
+    // const root = process.cwd().toString()
+
+    // for await (const output of build.outputs) {
+    //   const liam = output.path.replace(root + "/dist", "")
+
+    //   console.log("#", greyAndBold, output.hash)
+    //   console.log(hashEmoji, orange, liam)
+    // }
+    // console.log("=========================")
+    console.log(style, "Build succeeded", checkEmoji)
   } else {
-    console.log(error, "Build failed")
+    console.log(error, "Build failed", redXEmoji)
   }
 }
 
