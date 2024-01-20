802a14d1148513cccfa78edec0224365fb593757
GotPop <liam@gotpop.net>
2024-01-20 20:10:29 +0000

    Refactor watch.ts file

--- server/watch.ts
+++ server/watch.ts
@@ -1,31 +1,31 @@
-import type { FSWatcher } from "fs"
 import { Glob } from "bun"
+import type { FSWatcher } from "fs"
+import { watch } from "fs"
 import buildClientJSFiles from "./build"
 import { log } from "./logging"
-import { watch } from "fs"
 
 const glob = new Glob("**/*.client.ts")
 let watchers: FSWatcher[] = []
 
 for await (const file of glob.scan(".")) {
-    console.log('Watching:', file);
+  console.log("Watching:", file)
 
-    const watcher = watch(file, (event, filename) => {
-        if (event !== 'change') return
+  const watcher = watch(file, (event, filename) => {
+    if (event !== "change") return
 
-        log.updated(`${filename}: updated`);
-        buildClientJSFiles();
-    });
+    log.updated(`${filename}: updated`)
+    buildClientJSFiles()
+  })
 
-    watchers.push(watcher)
+  watchers.push(watcher)
 }
 
 process.on("SIGINT", () => {
-    console.log("Closing watchers...");
+  console.log("Closing watchers...")
 
-    for (const watcher of watchers) {
-        watcher.close();
-    }
+  for (const watcher of watchers) {
+    watcher.close()
+  }
 
-    process.exit(0);
-});
+  process.exit(0)
+})
