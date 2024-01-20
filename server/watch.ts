b04815f49c25b5bacad6b1b196c0b7678d90cfa6
GotPop <liam@gotpop.net>
2024-01-20 19:49:38 +0000

    Add file watchers for client TypeScript files

--- server/watch.ts
+++ server/watch.ts
@@ -1,35 +1,31 @@
+import type { FSWatcher } from "fs"
 import { Glob } from "bun"
 import buildClientJSFiles from "./build"
 import { log } from "./logging"
 import { watch } from "fs"
 
 const glob = new Glob("**/*.client.ts")
+let watchers: FSWatcher[] = []
 
 for await (const file of glob.scan(".")) {
     console.log('Watching:', file);
-    watch(file, (event, filename) => {
-        if (event === 'change') {
-            log.updated(`${filename}: updated`);
-            buildClientJSFiles();
-        }
+
+    const watcher = watch(file, (event, filename) => {
+        if (event !== 'change') return
+
+        log.updated(`${filename}: updated`);
+        buildClientJSFiles();
     });
+
+    watchers.push(watcher)
 }
 
-
-// const url = process.cwd() + "/src"
-
-// const watcher = watch(
-//     url,
-//     { recursive: true },
-//     (event, filename) => {
-//         // buildClientJSFiles()
-//         log.updated(`${filename}: updated`)
-//     }
-// )
-
 process.on("SIGINT", () => {
-    console.log(" Closing watcher...")
-    watcher.close()
+    console.log("Closing watchers...");
 
-    process.exit(0)
-})
+    for (const watcher of watchers) {
+        watcher.close();
+    }
+
+    process.exit(0);
+});
