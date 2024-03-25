7e5e941a3f4c5e1b3f75b1c18ca122585af010fa
GotPop <gotpop@users.noreply.github.com>
2024-03-25 10:32:35 +0000

    Add new CSS files and update Head component

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -2,15 +2,25 @@ import h from "@utils/jsxFactory"
 import { useCSS } from "src/hooks/useCSS"
 
 const Head = ({ title }: { title: string }) => {
-  const { css } = useCSS({ meta: import.meta, global: true })
+  const { css } = useCSS({
+    meta: import.meta,
+    global: true,
+  })
 
   return (
     <head>
       <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      />
       <title>{title}</title>
       <style>{css}</style>
       <link rel="icon" href="/assets/img/favicon.png" />
+      <link
+        rel="stylesheet"
+        href="/assets/styles/index.css"
+      />
     </head>
   )
 }
