fcf78d5ba49045b2dfa8ae4b98bfbe3ca2645f38
GotPop <gotpop@users.noreply.github.com>
2024-06-16 22:18:23 +0100

    refactor: Update Articles component to accept articles prop and remove unused code

---
+++ src/data/data.hero.ts
@@ -0,0 +1,4 @@
+export const heroData = {
+  title: "single dependency template",
+  text: "Singleton is a single dependency framework for building web applications.",
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,17 @@
-import AppFull from "@layouts/app-full"
-import Article from "@components/Article"
 import Articles from "@components/Articles"
+import { articles } from "@components/Articles/Articles.async"
 import Footer from "@components/Footer"
-import Gallery from "@components/Gallery"
 import GridFull from "@components/GridFull"
 import Header from "@components/Header"
 import Hero from "@components/Hero"
 import Points from "@components/Points"
-import { arr } from "@data/data"
+import { heroData } from "@data/data.hero"
+import AppFull from "@layouts/app-full"
 import { getPages } from "@utils/getPages"
 import h from "@utils/jsxFactory"
 
-export const buildArticles = async () => {
-  const galleryItem = await Gallery({
-    title: "Nice gallery",
-    content: "Nice gallery",
-  })
-
-  const promises = arr.map(
-    (item: { title: any; content: any }, i: number) => {
-      if (i === 2) {
-        return galleryItem
-      } else {
-        return Article({
-          title: item.title,
-          content: item.content,
-        })
-      }
-    }
-  )
-
-  const articles = await Promise.all(promises).then(
-    (article) => article.join("")
-  )
-
-  return articles
-}
-
 const indexPage = async () => {
-  const heroData = {
-    title: "single dependency template",
-    text: "Singleton is a single dependency framework for building web applications.",
-  }
-
   const pages = await getPages()
-  const articles = await buildArticles()
 
   return (
     <AppFull title="Home">
