904727bc942245fe31e7198218b405947a3d06da
GotPop <gotpop@users.noreply.github.com>
2024-06-17 13:50:03 +0100

    refactor: Remove commented out code in Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,6 @@ import { useCSS } from "src/hooks/useCSS"
 function Footer() {
   const { css, useName } = useCSS({ meta: import.meta })
 
-  // const brandsItem = await Brands({
-  //   title: "Brands",
-  //   text: "Brands",
-  // })
-
   return (
     <footer class={useName}>
       <style>{css}</style>
