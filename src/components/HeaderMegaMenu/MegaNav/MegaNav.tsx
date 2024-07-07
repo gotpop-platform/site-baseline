25cc6e2eece71fc4d001711f65917c73e3c67dbe
GotPop <gotpop@users.noreply.github.com>
2024-07-08 00:07:12 +0100

    refactor: Update MegaNav component to use data-driven approach
    This commit refactors the MegaNav component to use a data-driven approach for rendering navigation items and sub-menus. Instead of hardcoding the items in the component, we now use an array of data objects to dynamically generate the navigation items and their corresponding sub-menus. This improves the maintainability and flexibility of the MegaNav component, allowing for easier updates and additions to the navigation menu.

--- src/components/HeaderMegaMenu/MegaNav/MegaNav.tsx
+++ src/components/HeaderMegaMenu/MegaNav/MegaNav.tsx
@@ -10,30 +10,28 @@ import TriggerSubMenu from "../TriggerSubMenu"
 const MegaNav = () => {
   const { css } = useCSS({ meta: import.meta })
 
+  const data = [
+    { position: "1", textButton: "Products" },
+    { position: "2", textButton: "About" },
+    { position: "3", textButton: "Services" },
+  ]
+
+  const navItems = data.map((item) => (
+    <Fragment>
+      <TriggerSubMenu
+        position={item.position}
+        textButton={item.textButton}
+      />
+      <SubMenu position={item.position}>
+        <MegaNavContent />
+      </SubMenu>
+    </Fragment>
+  ))
+
   return (
     <nav class={mkClass(import.meta.file)}>
       <style>{css}</style>
-      <Fragment>
-        <TriggerSubMenu
-          position="1"
-          textButton="Products"
-        />
-        <SubMenu position="1">
-          <MegaNavContent />
-        </SubMenu>
-      </Fragment>
-      <Fragment>
-        <TriggerSubMenu position="2" textButton="About" />
-        <SubMenu position="2">
-          <ul>
-            <li class="item">Menu item 133</li>
-            <li class="item">Menu item 233</li>
-          </ul>
-        </SubMenu>
-      </Fragment>
-      <a class="trigger-sub-menu" href="/gallery">
-        Gallery
-      </a>
+      {navItems}
       <button
         class="trigger-sub-menu"
         popovertarget="basegrid"
