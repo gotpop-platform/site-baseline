1b2274fd99943fdcc93bf92c5bf1ff29c5965abe
GotPop <gotpop@users.noreply.github.com>
2024-07-18 17:23:03 +0100

    refactor: Update ArticlePage component to use dynamic heading level
    This commit updates the ArticlePage.tsx file to use a dynamic heading level based on the index of each section. Previously, the heading level was hardcoded as either 1 or 2. Now, the level is determined by the index, with the first section having a level of 1 and subsequent sections having a level of 2. This change improves the flexibility and scalability of the ArticlePage component, allowing for easier addition and rearrangement of sections.

--- src/themes/demo/components/ArticlePage/ArticlePage.tsx
+++ src/themes/demo/components/ArticlePage/ArticlePage.tsx
@@ -5,6 +5,8 @@ import h from "@utils/jsxFactory"
 import { mkClass } from "@utils/mkClass"
 import { useCSS } from "src/hooks/useCSS"
 
+const level = (index: number) => (index === 0 ? 1 : 2)
+
 function ArticlePage({
   section,
   style,
@@ -12,22 +14,16 @@ function ArticlePage({
   const { css } = useCSS({ meta: import.meta })
 
   const pageContent = section.map(
-    ({ title, content }, index) => {
-      const level = index === 0 ? 1 : 2
-
-      return (
-        <Fragment>
-          {
-            <Heading level={level} index={index}>
-              {title}
-            </Heading>
-          }
-          {content.map((p) => (
-            <p>{p}</p>
-          ))}
-        </Fragment>
-      )
-    }
+    ({ title, content }, index) => (
+      <Fragment>
+        <Heading level={level(index)} index={index}>
+          {title}
+        </Heading>
+        {content.map((p) => (
+          <p>{p}</p>
+        ))}
+      </Fragment>
+    )
   )
 
   return (
