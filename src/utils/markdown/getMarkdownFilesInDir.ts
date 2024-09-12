7834338bfe0358daa3711761fe5b17963e857893
GotPop <liam@gotpop.net>
2024-09-12 17:09:49 +0100

    Refactor getMarkdownFilesInDir.ts to use findMarkdownFiles instead of markdownFiles

--- src/utils/markdown/getMarkdownFilesInDir.ts
+++ src/utils/markdown/getMarkdownFilesInDir.ts
@@ -14,7 +14,7 @@ interface ParsedFile {
   content: string
 }
 
-export const markdownFiles = async (
+export const findMarkdownFiles = async (
   file: string
 ): Promise<string[]> => {
   const glob = new Glob("**/*.md")
@@ -36,27 +36,29 @@ export const markdownFiles = async (
 export const markdownFilesInDir = async (
   dir: string
 ): Promise<ParsedFile[]> => {
-  const awaitFiles: string[] = await markdownFiles(dir)
+  const foundFiles: string[] = await findMarkdownFiles(dir)
+
+  const mapFiles = foundFiles.map(
+    async (filePath): Promise<ParsedFile> => {
+      const path = filePath.replace(/\.md$/, "")
+      const { metadata, content } = parseMarkdownFile(
+        dir,
+        path
+      )
+
+      const typedMetadata: MetaData = {
+        title: metadata.title,
+        slug: metadata.slug,
+        author: metadata.author,
+        date: metadata.date,
+      }
+
+      return { metadata: typedMetadata, content }
+    }
+  )
 
   const parsedFiles: ParsedFile[] = await Promise.all(
-    awaitFiles.map(
-      async (filePath): Promise<ParsedFile> => {
-        const path = filePath.replace(/\.md$/, "")
-        const { metadata, content } = parseMarkdownFile(
-          dir,
-          path
-        )
-
-        const typedMetadata: MetaData = {
-          title: metadata.title,
-          slug: metadata.slug,
-          author: metadata.author,
-          date: metadata.date,
-        }
-
-        return { metadata: typedMetadata, content }
-      }
-    )
+    mapFiles
   )
 
   return parsedFiles
