75c594f5bd20191496dd23991cb904fd628ec924
GotPop <liam@gotpop.net>
2024-09-14 14:09:16 +0100

    Add BlogArticle component and fetch articles from markdown directory

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -9,11 +9,38 @@ import {
 
 import { AppTheme } from "layouts"
 import type { PageProps } from "types"
-import { jsxFactory } from "utils"
+import {
+  jsxFactory,
+  markdownFilesInDir,
+  type MetaData,
+} from "utils"
+
+const BlogArticle = ({
+  title,
+  slug,
+  author,
+  date,
+}: MetaData) => (
+  <article>
+    <a href={`blog/${slug}`}>
+      <h1>{title}</h1>
+      <p>
+        by {author} on {date}
+      </p>
+    </a>
+  </article>
+)
 
 const pageArticles = async ({
   slug,
 }: PageProps): Promise<JSX.Element> => {
+  const parsedFiles = await markdownFilesInDir("articles")
+
+  const articles = parsedFiles.map(
+    ({ metadata: { title, slug, author, date } }) => (
+      <ArticleItem title={title} slug={slug} date={date} />
+    )
+  )
   return (
     <AppTheme title={`Articles | ${slug}`}>
       <GridConfig>
