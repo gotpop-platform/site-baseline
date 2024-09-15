57355bffce6b3df9e10c21038386bf3c9bab4771
GotPop <liam@gotpop.net>
2024-09-15 17:09:15 +0100

    Refactor GalleryIntro component to use metadata and htmlContent, removing unused findPrevAndNext function

--- src/components/GalleryIntro/GalleryIntro.tsx
+++ src/components/GalleryIntro/GalleryIntro.tsx
@@ -1,51 +1,55 @@
 import { Icon } from "@components/Icon"
 import { IconTypes } from "@components/Icon/Icon"
-import { dataDemo, type GalleryItem } from "@data/demo.data"
-import { useCSS } from "@hooks/useCSS"
-import { mkClass } from "@utils/mkClass"
 import { jsxFactory } from "utils"
+import { mkClass } from "@utils/mkClass"
+// import { dataDemo, type GalleryItem } from "@data/demo.data"
+import { useCSS } from "@hooks/useCSS"
 
-export function findPrevAndNext(slug: string): GalleryItem {
-  const item = dataDemo.get(slug)
-  const init: GalleryItem = {
-    id: 1,
-    name: "Default Name",
-    prev: "item-4",
-    next: "item-2",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  }
+// export function findPrevAndNext(slug: string) {
+//   // const item = dataDemo.get(slug)
 
-  if (item) {
-    return {
-      ...item,
-    }
-  } else {
-    return init
-  }
-}
+//   const init: GalleryItem = {
+//     id: 1,
+//     name: "Default Name",
+//     prev: "item-4",
+//     next: "item-2",
+//     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+//   }
+
+//   if (item) {
+//     return {
+//       ...item,
+//     }
+//   } else {
+//     return init
+//   }
+// }
 
 export function GalleryIntro({
   slug,
+  metadata,
+  htmlContent,
 }: {
   slug: string
+  metadata: any
+  htmlContent: string
 }): JSX.Element {
   const { css, useName } = useCSS({ meta: import.meta })
-  const { prev, next, text } = findPrevAndNext(slug)
 
   return (
     <div class={mkClass(import.meta.file)}>
       <style>{css}</style>
-      <h2>Intro {slug}</h2>
-      <p>{text}</p>
+      <h2>{metadata.title}</h2>
+      <section>{htmlContent}</section>
       <nav class="links">
-        <a class="link" href={`/gallery/${prev}`}>
+        <a class="link" href={`/gallery/${metadata.prev}`}>
           <Icon
             type={IconTypes.twoTone}
             iconName="arrow_circle_left"
           />
           <span class="text">Prev</span>
         </a>
-        <a class="link" href={`/gallery/${next}`}>
+        <a class="link" href={`/gallery/${metadata.next}`}>
           <span class="text">Next</span>
           <Icon
             type={IconTypes.twoTone}
