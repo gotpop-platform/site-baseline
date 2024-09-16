393db8ac99df963eef676ca296b5266e41bb635b
GotPop <liam@gotpop.net>
2024-09-16 18:46:16 +0100

    Refactor HOC to utilize generic types for improved type safety; clean up unused code and comments

--- src/components/ArticleItem/HOC.tsx
+++ src/components/ArticleItem/HOC.tsx
@@ -1,15 +1,12 @@
 import { Fragment } from "../Fragment"
 import { jsxFactory } from "utils"
 
-// Define a generic type for data items
 export interface DataItem<T> {
   metadata: T
 }
 
-// Define a generic type for the component props
 type ComponentType<P> = (props: P) => JSX.Element
 
-// Define a generic interface for the HOC props
 interface WithItemsProps<T, P> {
   data: DataItem<T>[]
   layout: (
@@ -24,22 +21,17 @@ interface ExtendedProps extends Record<string, any> {
   ) => { [key: string]: string | number }[]
 }
 
-// Define the HOC function with generic types
 export function withItems<T, P extends ExtendedProps>(
   Component: ComponentType<P>
 ) {
   return function WrappedComponent({
     data,
     componentProps,
-  }: // componentProps,
-  WithItemsProps<T, P>) {
+  }: WithItemsProps<T, P>) {
     return (
       <Fragment>
         {data.map((item, index) => {
-          //   console.log("layout :", layout)
-          //   console.log("componentProps :", componentProps)
           const newLayout = componentProps(item).layout
-          console.log("newLayout :", newLayout)
 
           const props = {
             item,
