e578a0f1b33c7e9513a8ec847b90a277977fca6c
GotPop <liam@gotpop.net>
2024-09-17 11:16:55 +0100

    Rename parameter in stylesLayout function for improved clarity; update references to match new name

--- src/layouts/layouts.ts
+++ src/layouts/layouts.ts
@@ -1,30 +1,30 @@
 import type { MarkdownFileProps } from "utils"
 
 export const stylesLayout = (
-  item: MarkdownFileProps
+  markdownFile: MarkdownFileProps
 ): { [key: string]: string | number }[] => [
   {
     "--grid-column": "span 12",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
   {
     "--grid-column": "span 6",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
   {
     "--grid-column": "span 6",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
   {
     "--grid-column": "span 6",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
   {
     "--grid-column": "span 6",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
   {
     "--grid-column": "span 12",
-    "--transition-article": item.metadata.slug,
+    "--transition-article": markdownFile.metadata.slug,
   },
 ]
