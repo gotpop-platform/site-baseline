0c6cde708bb96227fa89f10a92c7958ca3c6b7a1
GotPop <liam@gotpop.net>
2024-09-22 00:47:34 +0100

    Update text-box-trim property to 'trim-both' in CSS files and refactor layoutBlog function to use metadata for slug

--- src/layouts/layouts.ts
+++ src/layouts/layouts.ts
@@ -32,35 +32,35 @@ export const stylesLayout = (
   },
 ]
 
-export const layoutBlog = (
-  markdownFile: MarkdownFileProps
-): StyleObjProps[] => [
+export const layoutBlog = ({
+  metadata,
+}: MarkdownFileProps): StyleObjProps[] => [
   {
     "--grid-column": "3 / span 13",
     "--grid-row": "2",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
   {
     "--grid-column": "18 / 37",
     "--grid-row": "2",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
   {
     "--grid-column": "3/ 37",
     "--grid-row": "4 / span 1",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
   {
     "--grid-column": "3/ 37",
     "--grid-row": "6 / span 1",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
   {
     "--grid-column": "span 6",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
   {
     "--grid-column": "span 12",
-    "--transition-article": markdownFile.metadata.slug,
+    "--transition-article": metadata.slug,
   },
 ]
