851244066986ed426821762ca10eae07cf286289
GotPop <liam@gotpop.net>
2024-09-23 22:01:09 +0100

    Refactor articlesSlug layout; enhance TableOfContents and PageContent components with improved styles and structure

--- src/layouts/articlesSlug.tsx
+++ src/layouts/articlesSlug.tsx
@@ -1,52 +1,54 @@
-import {
-    jsxFactory
-} from "utils";
+import { jsxFactory } from "utils"
 
-const PageContent = ({ htmlContent }: {htmlContent: string}) => (
-    htmlContent
-  )
-  
-  const TableOfContents = ({
-    toc,
-  }: {
-    toc?: { id: string; text: string }[]
-  }) => (
-    <nav>
-      <ul>
-        {toc?.map(({ id, text }) => (
-          <li>
-            <a href={"#" + id}>{text}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  )
-  
+const PageContent = ({
+  htmlContent,
+}: {
+  htmlContent: string
+}) => htmlContent
+
+const TableOfContents = ({
+  toc,
+}: {
+  toc?: { id: string; text: string }[]
+}) => (
+  <nav>
+    <ul>
+      {toc?.map(({ id, text }) => (
+        <li>
+          <a href={"#" + id}>{text}</a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+)
+
 export const layoutPages = (props: any) => [
-    {
-      type: "aside",
-      styles: {
-        "--background-color": "lightblue",
-        "--padding": "1rem",
-        "--grid-column": "3 / 7",
-        "--grid-row": "2",
-      },
-      props: {
-        ...props,
-      },
-      component: TableOfContents,
+  {
+    type: "aside",
+    styles: {
+      "--background-color": "lightblue",
+      "--padding": "1rem",
+      "--grid-column": "3 / 7",
+      "--grid-row": "2",
+      "--border-radius": "0.5rem  0 0 0.5rem",
     },
-    {
-      type: "section",
-      styles: {
-        "--background-color": "lightblue",
-        "--padding": "1rem",
-        "--grid-column": "9 / 37",
-        "--grid-row": "2",
-      },
-      props: {
-        ...props,
-      },
-      component: PageContent,
+    props: {
+      ...props,
     },
-  ]
+    component: TableOfContents,
+  },
+  {
+    type: "section",
+    styles: {
+      "--background-color": "lightblue",
+      "--padding": "1rem",
+      "--grid-column": "9 / 37",
+      "--grid-row": "2",
+      "--border-radius": "0 0.5rem 0.5rem 0",
+    },
+    props: {
+      ...props,
+    },
+    component: PageContent,
+  },
+]
