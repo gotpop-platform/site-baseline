6989b8ab7cd8e991530dd413a28e3975e84cd697
GotPop <liam@gotpop.net>
2024-10-01 14:15:07 +0100

    Update mkUrl function to use environment-specific base URLs for development and production

--- src/utils/mkUrl.ts
+++ src/utils/mkUrl.ts
@@ -1,2 +1,6 @@
+const isDev = process.env.NODE_ENV === "development"
+const live = process.env.RAILWAY_PUBLIC_DOMAIN
+const dev = process.env.BASE_URL
+
 export const mkUrl = (relative: string | URL) =>
-  new URL(relative, process.env.BASE_SITE_URL).href
+  new URL(relative, isDev ? dev : live).href
