3dafc35e226fdcb82e911f991c3a1ff6be1e2184
GotPop <liam@gotpop.net>
2024-10-04 23:18:55 +0100

    Update CSS properties and layout variables for improved consistency: modify padding syntax, adjust max-width for paragraphs, introduce new spacing variable, and refine background colors in article layout.

--- src/variables/articles.ts
+++ src/variables/articles.ts
@@ -7,7 +7,7 @@ export const layoutArticlesSlugSurface = {
 }
 
 export const layoutArticlesSlugToc = {
-  "--background-colour": "var(--grey-300)",
+  "--background-colour": "var(--grey-200)",
   "--display": "block",
   "--padding": "var(--spacing-sm)",
   "--grid-column": "1 gutter-start / 3 col-end",
@@ -15,9 +15,9 @@ export const layoutArticlesSlugToc = {
 }
 
 export const layoutArticlesSlugContent = (id: string) => ({
-  "--background-colour": "var(--grey-400)",
+  "--background-colour": "var(--grey-100)",
   "--display": "block",
-  "--padding": "var(--spacing-lg)",
+  "--padding": "var(--spacing-lg) var(--spacing-xxl)",
   "--grid-column": "4 gutter-start / -1 gutter-end",
   "--border-radius": "0 var(--radius-md) var(--radius-md) 0",
   "--transition-article": id,
