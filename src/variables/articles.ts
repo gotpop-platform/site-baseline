842aef8f8223ac35c48d3b0dd03fa2994dea6f48
GotPop <liam@gotpop.net>
2024-10-04 23:04:23 +0100

    Update workspace configuration and refactor layout styles for improved structure: add src folder to workspace, change Table of Contents position to sticky, and adjust background colors and grid properties in article layout variables.

--- src/variables/articles.ts
+++ src/variables/articles.ts
@@ -1,7 +1,7 @@
 export const layoutArticlesSlugSurface = {
-  "--background-colour": "var(--white)",
+  "--background-colour": "var(--transparent)",
   "--grid-column": "center",
-  "--grid-template-rows": "1rem 1fr 1rem",
+  "--grid-template-rows": "1fr",
   "--border-radius": "var(--radius-sm)",
   "--grid-row": "main",
 }
@@ -10,8 +10,7 @@ export const layoutArticlesSlugToc = {
   "--background-colour": "var(--grey-300)",
   "--display": "block",
   "--padding": "var(--spacing-sm)",
-  "--grid-column": "1 col-start / 2 col-end",
-  "--grid-row": "2",
+  "--grid-column": "1 gutter-start / 3 col-end",
   "--border-radius": "var(--radius-md) 0 0 var(--radius-md)",
 }
 
@@ -19,8 +18,7 @@ export const layoutArticlesSlugContent = (id: string) => ({
   "--background-colour": "var(--grey-400)",
   "--display": "block",
   "--padding": "var(--spacing-lg)",
-  "--grid-column": "3 col-start / -1 col-end",
-  "--grid-row": "2",
+  "--grid-column": "4 gutter-start / -1 gutter-end",
   "--border-radius": "0 var(--radius-md) var(--radius-md) 0",
   "--transition-article": id,
 })
