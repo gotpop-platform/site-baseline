256f037bb3659749bea0b7c638542371f434f86c
GotPop <liam@gotpop.net>
2024-10-16 22:38:38 +0100

    Refactor component page structure: update to use new styles, add documentation navigation, and implement code block rendering

--- src/pages/components/[slug].tsx
+++ src/pages/components/[slug].tsx
@@ -1,19 +1,35 @@
 import { AppTheme, Footer, GridGap, Heading, Metadata } from "@gotpop-platform/package-components"
-import { stylesBlog, stylesBlogInner } from "variables"
+import { stylesBlog, stylesBlogInner, stylesDocs, stylesDocsBody, stylesDocsNav } from "variables"
 
 import type { PageProps } from "types"
 import { SITE_NAME } from "src/constants"
 import { Tag } from "generics"
 import { jsxFactory } from "@gotpop-platform/package-jsx-factory"
-import { parseMarkdownFile } from "@gotpop-platform/package-markdown"
+import { CodeBlock, parseMarkdownFile } from "@gotpop-platform/package-markdown"
 import { title } from "@gotpop-platform/package-utilities"
 import { HeaderMegaMenu } from "src/com/HeaderMegaMenu"
+import { MenuSide } from "src/com/MenuSide"
+import Button from "@gotpop-platform/package-components/src/components/forms/Button"
 
-const pageBlog = async ({ slug }: PageProps): Promise<JSX.Element> => {
+const Fragment = ({ children }: { children?: JSX.Element }) => children || null
+
+const pageComponent = async ({ slug }: PageProps): Promise<JSX.Element> => {
   const {
     metadata: { date, title: pageTitle, author, id },
     content,
   } = parseMarkdownFile("components", slug)
+
+  const escapeHtml = (unsafe: string) => {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+  }
+
+  const jsxCode = `<LiamButton ignore="true">Click me!!!!!!!!!</LiamButton>`
+  const escapedCode = escapeHtml(jsxCode)
 
   return (
     <AppTheme title={title(pageTitle, SITE_NAME)}>
@@ -21,13 +37,27 @@ const pageBlog = async ({ slug }: PageProps): Promise<JSX.Element> => {
         <div class="graph">
           {/* <MobileMenuTrigger /> */}
           <HeaderMegaMenu />
-          <Tag tag="section" styles={stylesBlog}>
+          <Tag tag="main" styles={stylesDocs}>
+            <Tag tag="aside" class="docs-nav" styles={stylesDocsNav}>
+              <Fragment>
+                <MenuSide />
+              </Fragment>
+            </Tag>
+            {/* <ArticleList markdownItems={markdownItems} layout={stylesDocsLayout} /> */}
+            <Tag tag="section" class="docs-body" styles={stylesDocsBody}>
+              <Fragment>
+                <CodeBlock>{escapedCode}</CodeBlock>
+                {content}
+              </Fragment>
+            </Tag>
+          </Tag>
+          {/* <Tag tag="section" styles={stylesBlog}>
             <Tag tag="section" class="section-blog" styles={stylesBlogInner(id)}>
               <Heading>{pageTitle}</Heading>
               <Metadata date={date} author={author} />
               {content}
             </Tag>
-          </Tag>
+          </Tag> */}
           <Footer />
         </div>
       </GridGap>
@@ -35,4 +65,4 @@ const pageBlog = async ({ slug }: PageProps): Promise<JSX.Element> => {
   )
 }
 
-export default pageBlog
+export default pageComponent
