56b4ad44b5e03b771ad732dd904a2f3e1195088a
GotPop <liam@gotpop.net>
2024-11-02 22:02:51 +0000

    Add getPageMetadata function and pass metadata to MenuSide component

--- src/pages/components/[slug].tsx
+++ src/pages/components/[slug].tsx
@@ -11,15 +11,34 @@ import { stylesDocs, stylesDocsBody, stylesDocsNav } from "variables"
 
 import type { PageProps } from "types"
 import { SITE_NAME } from "src/constants"
-import { allContent } from '../../../server/serve'
+import { allContent } from "../../../server/serve"
 import { jsxFactory } from "@gotpop-platform/package-jsx-factory"
 import { title } from "@gotpop-platform/package-utilities"
 
 const Fragment = ({ children }: { children?: JSX.Element }) => children || null
 
+const getPageMetadata = (map: Map<string, any>): Map<string, any> => {
+  const result = new Map<string, any>()
+
+  for (const [key, value] of map.entries()) {
+    if (value.pageMetadata) {
+      result.set(key, value.pageMetadata)
+    }
+
+    if (value instanceof Map) {
+      result.set(key, getPageMetadata(value))
+    }
+  }
+
+  return result
+}
+
 const pageComponent = async ({ slug }: PageProps): Promise<JSX.Element> => {
   const { pageMetadata, htmlSectionsMap } = allContent.get("components").get(slug)
   const { finalContent } = await renderComponents(htmlSectionsMap.get("main"))
+
+  const allPageMetadata = getPageMetadata(allContent.get("components"))
+  console.log("allPageMetadata :", allPageMetadata)
 
   return (
     <AppTheme title={title(pageMetadata.title, SITE_NAME)}>
@@ -28,7 +47,7 @@ const pageComponent = async ({ slug }: PageProps): Promise<JSX.Element> => {
         <Tag tag="main" styles={stylesDocs}>
           <Tag tag="aside" class="docs-nav" styles={stylesDocsNav}>
             <Fragment>
-              <MenuSide />
+              <MenuSide allPageMetadata={allPageMetadata} />
             </Fragment>
           </Tag>
           <Tag tag="section" class="docs-body" styles={stylesDocsBody}>
