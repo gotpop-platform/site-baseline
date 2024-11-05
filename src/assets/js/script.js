b2e686652acb7654d85a9db8aa9f629f0b8fbf4c
GotPop <liam@gotpop.net>
2024-11-05 14:23:04 +0000

    Add grid painting functionality and resize observer for dynamic grid sizing

---
+++ src/assets/js/script.js
@@ -0,0 +1,13 @@
+function handleResize(entries) {
+  for (let entry of entries) {
+    const width = entry.contentRect.width
+
+    const rootFontSize = entry.target.computedStyleMap().get("font-size").value
+
+    entry.target.attributeStyleMap.set("--root-grid-size", rootFontSize)
+  }
+}
+
+const resizeObserver = new ResizeObserver(handleResize)
+
+resizeObserver.observe(document.documentElement)
