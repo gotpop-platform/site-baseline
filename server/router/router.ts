504fac7e127d31f049d782a48e7873685efcce24
GotPop <liam@gotpop.net>
2024-11-05 13:56:44 +0000

    Enhance logging with custom styles and improve error handling in handleGetPages function

--- server/router/router.ts
+++ server/router/router.ts
@@ -1,3 +1,5 @@
+import * as util from "node:util"
+
 import { BASE, router } from "../serve"
 import {
   INTERNAL_SERVER_ERROR_RESPONSE,
@@ -5,10 +7,14 @@ import {
   getBaseDomain,
   importModule,
 } from "./routerHelpers"
+import { inspect, styleText } from "node:util"
 
-export const handleGetPages = async (
-  request: Request
-): Promise<Response> => {
+// type StyleArrayType =
+
+// const styledLog = (stylesArray , message: string) => {
+//   const styles = stylesArray.map((style) => styleText(style, message))
+
+export const handleGetPages = async (request: Request): Promise<Response> => {
   try {
     const url = new URL(request.url)
     const baseUrl = new URL(BASE)
@@ -27,21 +33,41 @@ export const handleGetPages = async (
     }
 
     if (typeof module.default !== "function") {
-      console.error(
-        "Default export is not a function:",
-        module.default
-      )
+      console.error("Default export is not a function:", module.default)
       return INTERNAL_SERVER_ERROR_RESPONSE
     }
-    // console.log('route:', route);
-    
-    const response = await module
-      .default(route.query)
-      .catch((e: Error) => {
-        console.error("Error calling default export:", e)
 
-        return null
-      })
+    // Customize the styles
+    util.inspect.styles.string = "green"
+    util.inspect.styles.number = "blue"
+    util.inspect.styles.boolean = "red"
+    util.inspect.styles.date = "magenta"
+    util.inspect.styles.null = "bgCyan"
+    // util.inspect.styles.null = "blue"
+    util.inspect.styles.undefined = "bgYellow"
+
+    // Example object
+    const obj = {
+      name: "Alice",
+      age: 30,
+      names: null,
+      isAdmin: true,
+    }
+
+    // Inspect the object with custom styles
+    console.log(inspect(obj, { colors: true }))
+
+    const successMessage = styleText("green", "Success!")
+    console.log(successMessage)
+
+    const testMessage = styleText("bgMagenta", styleText("italic", "My italic underlined message"))
+    console.table(testMessage)
+
+    const response = await module.default(route.query).catch((e: Error) => {
+      console.error("Error calling default export:", e)
+
+      return null
+    })
 
     if (!response) {
       return NOT_FOUND_RESPONSE
