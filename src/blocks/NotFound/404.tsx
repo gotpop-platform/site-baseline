a94935ebdbd1b8527fc21bf14fe3a065ff44d311
GotPop <liam@gotpop.net>
2024-11-15 17:46:05 +0000

    Remove debug logging from blockNotFoundPage function

--- src/blocks/NotFound/404.tsx
+++ src/blocks/NotFound/404.tsx
@@ -10,8 +10,6 @@ export const blockNotFoundPage = async (data: {
   query: any
   scriptPaths: Record<string, string>[]
 }) => {
-  console.log("query :", data.query)
-
   return (
     <AppTheme title={title("Page Not Found", SITE_NAME)} scriptPaths={scriptPaths}>
       <GridGap isRoot>
