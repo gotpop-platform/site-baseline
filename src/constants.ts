235fc4165a31750e1555cceb335d42bf9437ab07
GotPop <liam@gotpop.net>
2024-11-16 15:11:25 +0000

    Remove unused constants from constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,5 @@
 // Define and export constants
 
-// export const PAGES_DIR = process.cwd() + "/src/pages"
-// export const PORT = 2000
-// export const PUBLIC_DIR = "./dist"
-// export const ROUTER_STYLE = "nextjs"
-// export const SITE_NAME = "Baseline"
-
-export const DATE_FORMAT_OPTIONS = {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-}
-
 export type DateFormatOptions = {
   year: "numeric"
   month: "long"
