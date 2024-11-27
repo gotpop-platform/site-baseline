b9a3b3fc0a9eb07c2f494f9f8c457ba606caec9a
GotPop <liam@gotpop.net>
2024-11-27 09:57:11 +0000

    Update workspace configuration and dependencies for improved project structure and compatibility

--- serve.ts
+++ serve.ts
@@ -1,11 +1,3 @@
 import { startServer } from "@gotpop-platform/package-server"
 
 startServer()
-
-const { env } = process
-// console.log("env :", env)
-const envEntries = Object.entries(env)
-const npmEnvArray = envEntries.filter(([key]) => key.startsWith("npm_package_config"))
-const npmEnv = Object.fromEntries(npmEnvArray)
-
-console.log(npmEnv)
