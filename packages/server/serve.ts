faf82cb8e1fa5bc48ca215c4f2c11e9ea23af8f2
GotPop <liam@gotpop.net>
2024-12-08 14:27:10 +0000

    Update package configurations and improve TypeScript settings for client and server

--- packages/server/serve.ts
+++ packages/server/serve.ts
@@ -1,22 +1,33 @@
 import { createCopyFilesPlugin, startServer } from "@gotpop-platform/package-baseline"
 
 import { env } from "process"
+import path from "path"
+
+// Get absolute paths
+const CLIENT_ROOT = path.resolve(__dirname, "../client/src")
+const PUBLIC_DIR = path.resolve(env.PROJECT_ROOT || "", env.npm_package_config_dir_public || "dist")
+// console.log("PUBLIC_DIR :", PUBLIC_DIR)
+const INPUT_DIR = path.resolve(CLIENT_ROOT, "assets")
+console.log("INPUT_DIR :", INPUT_DIR)
+const OUTPUT_DIR = path.resolve(PUBLIC_DIR, "assets")
+console.log("OUTPUT_DIR :", OUTPUT_DIR)
+
+// path.join(env.PROJECT_ROOT || "", "packages/client/dist/assets")
 
 const buildConfig = {
   entrypoints: [
-    "../client/src/assets/js/script.ts",
-    "../client/src/assets/js/worklets/worklet.grid.ts",
-    "../client/src/assets/js/worklets/worklet.hero.ts",
-    // "src/assets/styles/index.css", // CSS parsing is still patchy
+    path.join(CLIENT_ROOT, "assets/js/script.ts"),
+    path.join(CLIENT_ROOT, "assets/js/worklets/worklet.grid.ts"),
+    path.join(CLIENT_ROOT, "assets/js/worklets/worklet.hero.ts"),
   ],
-  outdir: env.npm_package_config_dir_public,
-  root: env.npm_package_config_root,
+  outdir: PUBLIC_DIR,
+  root: CLIENT_ROOT,
   naming: "[dir]/[name]-[hash].[ext]",
   experimentalCss: true,
   plugins: [
     createCopyFilesPlugin({
-      inputDir: "../client/src/assets",
-      outputDir: "../client/dist/assets",
+      inputDir: INPUT_DIR,
+      outputDir: OUTPUT_DIR,
       directories: ["fonts", "styles", "img"],
       preserveStructure: true,
       verbose: false,
