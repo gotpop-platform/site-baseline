d85f74ed671cac5266edad7d7dd5200e0a32a569
GotPop <liam@gotpop.net>
2024-12-08 15:03:51 +0000

    Update package-baseline dependency to version ^0.0.20 and simplify server TypeScript configuration

--- packages/server/serve.ts
+++ packages/server/serve.ts
@@ -8,9 +8,9 @@ const CLIENT_ROOT = path.resolve(__dirname, "../client/src")
 const PUBLIC_DIR = path.resolve(env.PROJECT_ROOT || "", env.npm_package_config_dir_public || "dist")
 // console.log("PUBLIC_DIR :", PUBLIC_DIR)
 const INPUT_DIR = path.resolve(CLIENT_ROOT, "assets")
-console.log("INPUT_DIR :", INPUT_DIR)
+// console.log("INPUT_DIR :", INPUT_DIR)
 const OUTPUT_DIR = path.resolve(PUBLIC_DIR, "assets")
-console.log("OUTPUT_DIR :", OUTPUT_DIR)
+// console.log("OUTPUT_DIR :", OUTPUT_DIR)
 
 // path.join(env.PROJECT_ROOT || "", "packages/client/dist/assets")
 
