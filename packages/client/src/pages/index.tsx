2bc1ae716310104de6c6e8fdf9e3e749a4cdf3ca
GotPop <liam@gotpop.net>
2024-12-09 00:09:49 +0000

    Remove commented-out code from pageIndex function in index.tsx

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -2,13 +2,6 @@ import { BlockDataProps } from "src/types/types"
 import { block } from "blocks"
 
 const pageIndex = async (data: BlockDataProps): Promise<JSX.Element> => {
-  // console.log("data :", data)
-  const { slug } = data.query
-
-  // // Handle home page
-  // if (slug === "/") {
-  //   return block({ slug: "/" })
-  // }
 
   return block(data)
 }
