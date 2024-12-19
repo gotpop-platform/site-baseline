d01aa712139489dfc56ac6ad2fcac2e87921a625
GotPop <liam@gotpop.net>
2024-12-19 16:19:05 +0000

    Add Bun project configuration files and update workspace settings

---
+++ .vscode/code-style.md
@@ -0,0 +1,13 @@
+This is a Bun project
+
+This is a personal project
+
+This project will be using experimental web platform features
+
+This project uses Bun and no other libraries 
+
+This project uses a single package which is also only using Bun with no additional libraries
+
+This project does not use React
+
+This project uses JSX provided natively by Bun
--- packages/client/blocks/NotFound/404.tsx
+++ packages/client/blocks/NotFound/404.tsx
@@ -1,27 +1,18 @@
-import { AppTheme, Footer, GridGap, HeaderMegaMenu, Tag, jsxFactory, title } from "@gotpop-platform/package-baseline"
+import { Tag, jsxFactory, title } from "@gotpop-platform/package-baseline"
 import { layoutArticlesSlugSurface, stylesBlog } from ".."
 
 import { BlockDataProps } from "@gotpop-platform/types"
+import { Layout } from "../Layout" // Import Layout component
 
 export const blockNotFoundPage = async (data: BlockDataProps): Promise<any> => {
   return (
-    <AppTheme
-      title={title("Page Not Found")}
-      scriptPaths={data.scriptPaths}
-    >
-      <GridGap isRoot>
-        <div class="graph">
-          {/* <MobileMenuTrigger /> */}
-          <HeaderMegaMenu />
-          <Tag tag="main" styles={layoutArticlesSlugSurface}>
-            <Tag tag="section" styles={stylesBlog}>
-              <h1>404 - Page Not Found</h1>
-              <p>Sorry, the page you are looking for does not exist.</p>
-            </Tag>
-          </Tag>
-          <Footer />
-        </div>
-      </GridGap>
-    </AppTheme>
+    <Layout title={title("Page Not Found")} scriptPaths={data.scriptPaths}>
+      <Tag tag="main" styles={layoutArticlesSlugSurface}>
+        <Tag tag="section" styles={stylesBlog}>
+          <h1>404 - Page Not Found</h1>
+          <p>Sorry, the page you are looking for does not exist.</p>
+        </Tag>
+      </Tag>
+    </Layout>
   )
 }
